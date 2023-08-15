a3952d1f2333cf52611e110ee74e034050c5bbf5
Jason Harris <lord.jtharris@gmail.com>
2023-08-14 22:57:10 -0500

    Fixed my horrible late night logic and added region select value to storage in the save handler.

--- modules/Save-Settings.js
+++ modules/Save-Settings.js
@@ -4,11 +4,13 @@ document.addEventListener("click", e => {
         e.preventDefault();
         e.stopPropagation();
         const oldValue = chrome.storage.local.get("gold-cap");
+        const oldRegion = chrome.storage.local.get("wow-region");
 
-        if(e.target.value === typeof("number")){
-            chrome.storage.local.set({"gold-cap": e.target.value})
+            chrome.storage.local.set({"gold-cap": document.getElementById("pw-gold-input-field").value})
+            chrome.storage.local.set({"wow-region": document.getElementById("pw-preferred-region-select").value})
             console.log(`The user will now be notified when the price of a WoW Token is at or below ${chrome.storage.local.get("gold-cap")}.`)
             console.log(`Previous value was ${oldValue}`);
-        }
+            console.log(`------------`);
+            console.log(`Saved region is: ${chrome.storage.local.get("wow-region")}. Old region was ${oldRegion}`);
     }
 })
